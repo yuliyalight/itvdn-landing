da3c2c14dbc2c1c8f196535e0b3f93ad8480a69a
yuliyalight <usved5678@gmail.com>
2018-11-13 19:31:13 +0200

    Add sections

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,9 @@ var sass = require('gulp-sass');
 var spritesmith = require('gulp.spritesmith');
 var rimraf = require('rimraf');
 var rename = require("gulp-rename");
-
-
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
+var sourcemaps = require("gulp-sourcemaps");
 
 
 /*--------Server--------*/
@@ -37,6 +38,22 @@ gulp.task('styles:compile', function () {
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename('main.min.css'))
         .pipe(gulp.dest('build/css'));
+});
+
+/*--------js--------*/
+gulp.task('js', function () {
+    return gulp.src([
+        'source/js/init.js',
+        'source/js/validation.js',
+        'source/js/form.js',
+        'source/js/navigation.js',
+        'source/js/main.js'
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('build/js'));
 });
 
 /*--------Sprite--------*/
@@ -76,11 +93,12 @@ gulp.task('copy', gulp.parallel('copy:fonts', 'copy:images'));
 gulp.task('watch', function() {
     gulp.watch('source/template/**/*.pug', gulp.series('templates:compile'));
     gulp.watch('source/styles/**/*.scss', gulp.series('styles:compile'));
+    gulp.watch('source/js/**/*.js', gulp.series('js'));
 });
 
 gulp.task('default', gulp.series(
     'clean',
-    gulp.parallel('templates:compile', 'styles:compile', 'sprite', 'copy'),
+    gulp.parallel('templates:compile', 'styles:compile', 'js', 'sprite', 'copy'),
     gulp.parallel('watch', 'server')
     )
 );
---
+++ source/js/main.js
@@ -0,0 +1,37 @@
+(function () {
+    // Add event listener for open form button
+    var openFormButton = document.querySelector('.arrow-down');
+    var form = document.querySelector('.form');
+    var nav = document.querySelector('.nav');
+
+    if (openFormButton) {
+        openFormButton.addEventListener('click', function (e) {
+            e.preventDefault();
+            ITVDN.form.open();
+        })
+    }
+
+    if (form) {
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            if (ITVDN.form.isValid()) {
+                console.log('All good');
+            } else {
+                console.log('Is not valid');
+            }
+        })
+    }
+
+    if (nav) {
+        nav.addEventListener('click', function(e) {
+           var target = e.target;
+
+           if (target.tagName.toLowerCase() !== 'a') {
+               return;
+           }
+           e.preventDefault();
+            ITVDN.navigation.toggleToActiveLink(target);
+        });
+    }
+
+}());
